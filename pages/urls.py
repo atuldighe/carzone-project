ed8999afe7e796a5b42f279c10ffb8be8a448080
atuldighe <atuldighe@gmail.com>
2022-08-19 23:09:57 +0530

    Committed as Added POST GRES SQL Settings and Bootstrap

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.about, name='about'),
+    path('', views.services, name='services'),
+    path('', views.contact, name='contact'),
 ]
